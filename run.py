0bef77b4ea27e32bdd05d0014f912d6bf7576e5f
hussein18149 <husseinkatana17@gmail.com>
2020-07-26 15:57:16 +0300

    named csv columns

--- run.py
+++ run.py
@@ -8,9 +8,6 @@ app = Flask(__name__)
 def my_home():
     return render_template('index.html')
 
-
-# def my_home():
-#     return render_template('index.html')
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
